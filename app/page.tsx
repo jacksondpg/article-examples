54edfa9dace6d0d54e44a5d81f74144beefcb14d
jacksondpg <jackson@grupodpg.com.br>
2025-12-29 12:12:04 -0300

    add article

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       {articles.map((article) => (
-        <div key={article.id} style={{ marginBottom: 40 }}>
+        <div key={article.id} style={{ marginBottom: 40 }} className="divHome">
           <h2>{article.payload.theme}</h2>
 
           <pre>{JSON.stringify(article.payload, null, 2)}</pre>
